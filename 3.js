1900fdb8476242aed4807da849bcf35033329043
FFire <8497715@gmail.com>
2022-12-03 04:31:23 +0500

    remove old files

--- 3.js
+++
@@ -1,9 +0,0 @@
-// 3. Дан массив ['spaghetti','pizza', 'risotto', ‘lasagna’];
-// - Добавить посередине элемент ‘carbonara’ и вывести в консоль (console.log) результат
-
-const arr = ["spaghetti", "pizza", "risotto", "lasagna"];
-const item = "carbonara";
-
-console.log("Source array:", arr);
-arr.splice(2, 0, item);
-console.log("Array with new item:", arr);
